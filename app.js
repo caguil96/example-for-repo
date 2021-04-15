7599e5debe75690929c8ee951df5494fdcaa0a47
Leonardo Aguilar <Leonardoaguilar@leonardos-air.lan>
2021-04-15 16:31:26 -0400

    new commit

--- app.js
+++ app.js
@@ -34,19 +34,50 @@ function setOptions(list) {
     optionTag.value = breed;
     selectTag.append(optionTag);
   })
+  return list;
 }
-
 // Get option tag value
+function getValue(e) {
+  e.preventDefault();
+  removeImage();
+  const optionValue = document.querySelector('#select-breed').value;
+  console.log(optionValue);
 
-
+}
 // Form Eventhandler
-
+const form = document.querySelector('form');
+form.addEventListener("submit", getValue);
 
 // API request for random dog image
+// getDogImage https://dog.ceo/api/breed/breed/images/random
+async function getBreedImage(breedValue) {
+  // console.log("Right here", breedValue);
+  // URL https://dog.ceo/api/breed/breed/images/random
+  try {
+    const imageResponse = await axios.get(`https://dog.ceo/api/breed/breed/${breedValue}/images/random`);
+    console.log(imageResponse.data.message);
+    const imageURL = imageResponse.data.message;
+    appendImage(imageURL);
+    return imageURL;
+  } catch (error) {
+    console.error(error);
+  }
 
+}
 
 // Create dynamic image tag and append to DOM
-
+function appendImage(imageURL) {
+  const imageDiv = document.querySelector('#dog-image');
+  console.log(imageSrc);
+  const img = document.createElement('img');
+  img.src = imageSrc;
+  imageDiv.append(image);
+}
 
 // Remove previous dog image
-
+function removeImage() {
+  const removeImageDiv = document.querySelector('#dog-image');
+  while (removeImageDiv.lastChild) {
+    removeImageDiv.removeChild(removeImage.lastChild);
+  }
+}
