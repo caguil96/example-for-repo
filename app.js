6291cb8259aefceb80b23daee25eaea7c0002474
Leonardo Aguilar <Leonardoaguilar@leonardos-air.lan>
2021-04-15 14:28:01 -0400

    First step complete

---
+++ app.js
@@ -0,0 +1,37 @@
+// Add HTML connect all files
+// Completed 
+
+// Create dynamic dropdown menu from GET all dogs
+// https://dog.ceo/api/breeds/list/all
+
+const getOptions = async () => {
+  const url = https://dog.ceo/api/breeds/list/all
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+
+  } catch (error) {
+    console.error(error);
+
+  }
+  const response = await axios.get(url);
+}
+getOptions
+
+// Create the form option tags
+
+
+// Get option tag value
+
+
+// Form Eventhandler
+
+
+// API request for random dog image
+
+
+// Create dynamic image tag and append to DOM
+
+
+// Remove previous dog image
+
